43af9c0a9b92f85b20060293450146ae9e5cbb37
Aitassova Mereilim <mereilim@Aitassovas-MacBook-Pro.local>
2020-11-12 16:11:30 -0500

    Fully Functional App

--- SOCKET-IO/Creating-Handshake/client/src/App.js
+++ SOCKET-IO/Creating-Handshake/client/src/App.js
@@ -1,34 +1,93 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+// import React, { useState, useEffect } from 'react';
+// import io from 'socket.io-client';
+// import './App.css';
+// import './components/style.css'
+
+// import Chat from './components/Chat';
+// import Welcome from './components/Welcome';
+// import Header from './components/Header';
+
+// function App() {
+//   // notice that we pass a callback function to initialize the socket
+//   // we don't need to destructure the 'setSocket' function since we won't be updating the socket state
+//   const [socket] = useState(() => io(':8000'));
+
+//   useEffect(() => {
+//     // we need to set up all of our event listeners
+//     // in the useEffect callback function
+//     console.log('Is this running?');
+//     socket.on('Welcome', data => console.log(data));
+
+//     // note that we're returning a callback function
+//     // this ensures that the underlying socket will be closed if App is unmounted
+//     // this would be more critical if we were creating the socket in a subcomponent
+//     return () => socket.disconnect(true);
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <Header/>
+//       <Welcome/>
+//       <Chat/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React,{useState,useEffect} from 'react';
 import './App.css';
-import './components/style.css'
+import io from 'socket.io-client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Chat from './components/Chat';
-import Welcome from './components/Welcome';
-import Header from './components/Header';
-
-function App() {
-  // notice that we pass a callback function to initialize the socket
-  // we don't need to destructure the 'setSocket' function since we won't be updating the socket state
-  const [socket] = useState(() => io(':8000'));
+const App = () => {
+  const [name,setName] = useState("");
+  const [socket] = useState(() => io(":8000"));
+  const [input,setInput] = useState("");
+  const [messages, setMessages] = useState([])
 
   useEffect(() => {
-    // we need to set up all of our event listeners
-    // in the useEffect callback function
-    console.log('Is this running?');
-    socket.on('Welcome', data => console.log(data));
+    socket.on("Welcome", data => console.log(data));
+    socket.on("joined", data => console.log(data));
+    socket.on("sent",data => console.log(data));
 
-    // note that we're returning a callback function
-    // this ensures that the underlying socket will be closed if App is unmounted
-    // this would be more critical if we were creating the socket in a subcomponent
+    socket.on("updatingMessages", data => setMessages(data));
     return () => socket.disconnect(true);
-  }, []);
+  },[socket])
 
+  const sendToServer = () => {
+    socket.emit("addToChat",`${name} says: ${input}`);
+    setInput("");
+  }
   return (
-    <div className="App">
-      <Header/>
-      <Welcome/>
-      <Chat/>
+    <div className="App col-5 mx-auto">
+      <div className="form-group">
+          <label>name:</label>
+          <input 
+            type="text" 
+            value={name} 
+            onChange={(e) => setName(e.target.value)} 
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Send a message:</label>
+          <input 
+            type="text" 
+            value={input} 
+            onChange={(e) => setInput(e.target.value)} 
+            className="form-control"
+          />
+        </div>
+        <button  onClick={sendToServer} className="btn btn-warning">Send</button>
+
+
+        <ul className="list-group">
+          {
+            messages.map((m,i) => <li key={i} className="list-group-item">{m}</li>)
+          }
+        </ul>
     </div>
   );
 }
--- SOCKET-IO/Creating-Handshake/server.js
+++ SOCKET-IO/Creating-Handshake/server.js
@@ -1,24 +1,56 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const server = app.listen(8000, ()=>{
-    console.log("The server is all fired up on port 8000")
-})
+// const server = app.listen(8000, ()=>{
+//     console.log("The server is all fired up on port 8000")
+// })
 
-//Initiaize socket
-const io = require("socket.io")(server);
+// //Initiaize socket
+// const io = require("socket.io")(server);
 
 
-io.on("connection", socket =>{
+// io.on("connection", socket =>{
+//     console.log(socket.id);
+//     console.log("Nice to meet you. (shake hand)")
+
+
+//     io.emit('check 123')
+//     socket.emit('check 456');
+//     console.log('emits done')
+//     socket.on("event_from_client", data =>{
+//         socket.broadcast.email("send_data_to_all_other_clients", data)
+//     })
+
+// })
+
+
+const express = require('express'),
+    port = 8000,
+    app = express(),
+    cors = require('cors'),
+    server = app.listen(port,() => console.log(`Listening on port ${port}`)),
+    io = require('socket.io')(server);
+
+
+app.use(cors());
+
+
+const chats = [];
+
+
+io.on('connection', socket => {
     console.log(socket.id);
-    console.log("Nice to meet you. (shake hand)")
 
+    socket.broadcast.emit("joined","another client joined the chat");
 
-    io.emit('check 123')
-    socket.emit('check 456');
-    console.log('emits done')
-    socket.on("event_from_client", data =>{
-        socket.broadcast.email("send_data_to_all_other_clients", data)
+    //emit sends data only to the client that sent the event
+    socket.emit("Welcome", {msg:"Hello from the server"});
+
+    socket.on("addToChat",data => {
+        socket.emit("sent","You send a message");
+        chats.push(data);
+
+        // sends to all clients
+        io.emit("updatingMessages",chats);
     })
-
 })
